b15ce45e41c3aef1299c8f31d4c055c949e05c84
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-04-02 22:14:43 -0400

    simple scraper

---
+++ beginner-scraping.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
+
+def scrape():
+    query="software engineering internships near Toronto Ontario"
+
+    for result in search(query, num_results=10):
+        url = result
+        print(f"Scraping: {url}")
+
+        try: 
+            response = requests.get(url, timeout=5)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            title = soup.select_one('h1').text if soup.select_one('h1') else "No <h1> found"
+            text = soup.select_one('p').text if soup.select_one('p') else "No <p> found"
+            link = soup.select_one('a').get('href') if soup.select_one('a') else "No <a> found"
+
+            
+            print("Title:", title)
+            print("Snippet:", text)
+            print("First link:", link)
+
+        except Exception as e:
+            print(f"There was an error scraping {url}: {e}")
+
+if __name__ == '__main__':
+    scrape()
+
+
